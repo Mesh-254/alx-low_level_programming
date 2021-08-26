7dad37319e4b6ee6bd8c6c97fcb2a5b31902e5f9
Mesh-254 <meshack1995@gmail.com>
2021-08-26 11:18:17 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
+
+/*This is my first C program*/
 #include <stdio.h>
+
+/**
+ *main -entry point
+ *
+ *return: always zero
+ */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
