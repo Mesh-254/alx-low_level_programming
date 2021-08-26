0a3d58856f8a2b484244951248f529e663d8ca00
Mesh-254 <meshack1995@gmail.com>
2021-08-26 13:52:14 -0700

    print size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main: entry point
- *
+ *Description: this program prints out size of various types
  */
 int main(void)
 {
@@ -10,6 +10,8 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-/*Return: 0*/
+/**
+ *Return: 0
+ */
 return (0);
 }
