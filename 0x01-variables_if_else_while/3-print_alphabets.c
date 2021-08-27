bcb818bb833e3306aa038188c057c1995f0a63cc
Mesh-254 <meshack1995@gmail.com>
2021-08-27 01:17:23 -0700

    lowercase and uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main: main block
+ *Description: Print alphabets in upper and lower cases
+ *
+ *Return: 0
+ */
+int main(void)
+{
+char ch;
+char CH;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+
+putchar(ch);
+
+for (CH = 'A'; CH <= 'Z'; CH++)
+
+putchar(CH);
+
+putchar('\n');
+return (0);
+}
