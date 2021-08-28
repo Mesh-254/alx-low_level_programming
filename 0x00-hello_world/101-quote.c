5c1e5d5591d12b40b567cd6f86751c046e9e5424
Mesh-254 <meshack1995@gmail.com>
2021-08-28 00:02:06 -0700

    Printing output followed by standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-9", 59);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-9");
 return (1);
 }
