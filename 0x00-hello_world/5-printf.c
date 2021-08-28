8b51dd9f05574c12e8ef0b8f259e89ca984a91b3
Mesh-254 <meshack1995@gmail.com>
2021-08-27 22:07:45 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- *
- *Main- main function/Entry point
+ *Main: main function/Entry point
  *
  *return: 0
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
