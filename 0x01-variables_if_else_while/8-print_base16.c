11029b8c2f5d19ce715648b9268264c3800a4e63
Mesh-254 <meshack1995@gmail.com>
2021-08-30 03:52:54 -0700

    printing numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 /**
- *main: main block
- *description: print hexadecimal numbers in lowercase
- *return: 0
- *
+ * main - Entry point
+ * description: print hexadecimal numbers in lowercase
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i;
 int ch;
-
 for (i = '0'; i <= '9'; i++)
 putchar(i);
-for (ch ='a'; ch <= 'f'; ch++)
+for (ch = 'a'; ch <= 'f'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
-
 }
