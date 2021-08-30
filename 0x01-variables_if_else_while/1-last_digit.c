e436f6a39a882751580c03e0b3bf3ce16df40564
Mesh-254 <meshack1995@gmail.com>
2021-08-30 02:52:57 -0700

    Assigning randon number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
+ * main - checks last digit
  *
- *Main: checks last digit
- *
- *Return: 0
- *
+ * descritpion - Assigning random number to variable n
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int n;
-  int last;
-
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  last = n % 10;
-
+int n;
+int last;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
 if (last > 5)
-printf("The last digit of %d is %d and is greater than 5\n", n, last);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 else if (last == 10)
 printf("Last digit of %d is %d and is 0\n", n, last);
-else if (last < 6 && last !=0)
-printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last);
+else if (last < 6 && last != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 return (0);
 }
-  
