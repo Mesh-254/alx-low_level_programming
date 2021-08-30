de0c83f301ddbe92ddc26f39c8bfb10ffc535366
Mesh-254 <meshack1995@gmail.com>
2021-08-30 03:26:54 -0700

    printing all single digit numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,22 +2,19 @@
 #include <stdlib.h>
 
 /**
- *main: Entry point
- *description: printing single digit numbers of base10
- *return: 0
- *
+ * main - entry point
+ * description - printing single digit numbers of base10
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 char c = '0';
-
 while (c <= '9')
 {
 putchar(c);
 c++;
 }
 putchar('\n');
- 
 return (0);
 }
