9c3df6085de6746f689afd8c2ea547f83b692b6b
Mesh-254 <meshack1995@gmail.com>
2021-08-30 04:06:51 -0700

    prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- *main: main block
- *description: printing combination of 3 different digits
- *return: 0
+ * main - entry point
+ * description: printing combination of 3 different digits
+ * Return: always 0 (Success)
  */
 
 int main(void)
@@ -15,18 +15,18 @@ int e = 0;
 
 while (e < 10)
 {
-d =0;
-while (d <10)
+d = 0;
+while (d < 10)
 {
 c = 0;
 while (c < 10)
 {
-if (c !=d && d !=e && e < d && d < c)
+if (c != d && d != e && e < d && d < c)
 {
 putchar('0' + e);
 putchar('0' + d);
 putchar('0' + c);
-if(c + d + e != 9 + 8 + 7)
+if (c + d + e != 9 + 8 + 7)
 {
 putchar(',');
 putchar(' ');
