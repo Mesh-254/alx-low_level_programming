2edf0f5cba38b89366157f13dcef79e4ea6968b7
Mesh-254 <meshack1995@gmail.com>
2021-08-31 08:22:35 -0700

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - check for lower case letter
+ * @c: character to check for lowercase letters
+ * Return: always (0) or (1)
+ */
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
