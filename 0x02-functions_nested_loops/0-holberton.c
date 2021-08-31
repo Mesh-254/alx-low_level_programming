ab05d38843c6c334d95c1f9c9b7874bb393aa076
Mesh-254 <meshack1995@gmail.com>
2021-08-31 07:51:41 -0700

    prints Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-char c[10] = "Holberton";
-int i = 0;
-while (i < 9)
-{
-_putchar(c[i]);
-i++;
-}
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
 return (0);
 }
