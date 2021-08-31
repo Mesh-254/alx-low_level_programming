95bf47bdb72c152a2eeddcfb2a09cbe323db92b9
Mesh-254 <meshack1995@gmail.com>
2021-08-31 04:08:53 -0700

    printing holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+char c[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
