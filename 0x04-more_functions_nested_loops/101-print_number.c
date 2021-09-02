41d73ef94f2dd5217a14f8f7e3b37cd42b69674f
Mesh-254 <meshack1995@gmail.com>
2021-09-02 06:28:12 -0700

    function prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,25 @@
  */
 void print_number(int n)
 {
+int d = 1, i = 0, ii = 0;
 if (n < 0)
 {
 _putchar('-');
 n = -n;
 }
-if (n / 10 != 0)
+while (n / d != 0)
 {
-print_number(n / 10);
+d *= 10;
+i++;
 }
-_putchar(n % 10 + '0');
+d = d / 10;
+while (ii < i)
+{
+_putchar('0' + n / d);
+n = n - (n / d) *d;
+d = d / 10;
+i++;
+}
+if (i == 0)
+_putchar('0' + n);
 }
