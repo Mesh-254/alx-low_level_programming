ef41c2adb1c4969dad96da850769cbebcdaa4fee
Mesh-254 <meshack1995@gmail.com>
2021-09-06 00:02:18 -0700

    swaps the values of two integers@

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * *@a: the first integer argument 
+ * *@b: second integer argument
+ * return: values of a and b
+ */
+void swap_int(int *a, int *b)
+{
+int i;
+
+i  = *a;
+*a = *b
+*b = i;
+}
