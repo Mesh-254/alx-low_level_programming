402a8f092c43b0312f30754b25308085f725a819
Mesh-254 <meshack1995@gmail.com>
2021-09-09 07:42:44 -0700

    function that prints an integer.@

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,15 @@
  */
 void print_number(int n)
 {
-int divisor = 1, i, resp;
+
+unsigned int x = n;
+
 if (n < 0)
 {
 _putchar('-');
-n *= -1;
+x = -x;
 }
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-resp = n / divisor;
-_putchar('0' + resp);
-}
+if ((x / 10) > 0)
+print_number(x / 10);
+_putchar(x % 10 + '0');
 }
