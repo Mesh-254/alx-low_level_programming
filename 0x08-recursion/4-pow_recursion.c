53bf0b52793479c229a966b18640c2d3c0a71fce
Mesh-254 <meshack1995@gmail.com>
2021-09-15 07:39:27 -0700

    function that returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,6 +3,7 @@
  * _pow_recursion -  returns the value of x raised to the power of y
  * @x: integer
  * @y: integer
+ * Return: integer
  */
 int _pow_recursion(int x, int y)
 {
@@ -11,5 +12,5 @@ return (-1);
 else if (y == 0)
 return (1);
 else
-return (x * _pow_recursion(x, y-1));
+return (x * _pow_recursion(x, y - 1));
 }
