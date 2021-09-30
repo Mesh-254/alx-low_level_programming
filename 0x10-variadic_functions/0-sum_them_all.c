b9bff8d2528bab5285ef7fc07efe0f659ab1d8c7
Mesh-254 <Meshack1995@gmail.com>
2021-09-30 13:38:48 -0700

    Update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,9 +10,9 @@ int sum_them_all(const unsigned int n, ...)
 va_list list;
 unsigned int i;
 double sum = 0;
+va_start(list, n);
 if (n == 0)
 return (0);
-va_start(list, n);
 for (i = 0; i < n; i++)
 {
 sum += va_arg(list, int);
