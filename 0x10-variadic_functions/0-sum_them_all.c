1cf8ed08342c34b18f8b0d6a550ba7fe53e4e307
Mesh-254 <Meshack1995@gmail.com>
2021-09-30 12:10:05 -0700

    returns the sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,18 +7,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
 va_list list;
-double sum = 0;
+unsigned int sum = 0, i;
 if (n == 0)
-{
 return (0);
-}
 va_start(list, n);
 for (i = 0; i < n; i++)
-{
 sum += va_arg(list, int);
-}
 va_end(list);
 return (sum);
 }
