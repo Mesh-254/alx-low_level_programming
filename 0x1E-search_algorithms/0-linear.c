6aa97d1431e852e2579d48e1d6bd0efa7f3f308d
Mesh-254 <Meshack1995@gmail.com>
2022-03-30 02:58:42 -0700

    unction that searches for a value in an array of integers

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,25 +1,30 @@
 #include "search_algos.h"
 
 /**
- * linear_search - implement  linear search  algo
- * @array: given array to search in
- * @size: size of the array
- * @value: value to search for
- * Return: first index or -1
+ * linear_search - unction that searches for a value in an array of integers
+ * using the Linear search algorithm
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: is the value to search for
+ * Return: return the first index where value is located, If value is not
+ * present in array or if array is NULL, your function must return -1
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
 	if (array == NULL)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (i);
-
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+			return ((int)i);
+		}
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
 	}
 	return (-1);
-
 }
